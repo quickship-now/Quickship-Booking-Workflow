f214f2a09b88367a150bea0a099b34b2203c2bd0
quickship-now <info@quickshipnow.com>
2026-07-25 11:33:28 +0530

    Update WORKFLOW_API_URL in config.js

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-window.WORKFLOW_API_URL = "https://script.google.com/macros/s/AKfycbxKUc2H3yiwfqoazXwlvWgjM7t7mU3lVXOoyARJ5liM3XjbdMBxsNP8MVrBF6hiKz8tCQ/exec";
+window.WORKFLOW_API_URL = "https://script.google.com/macros/s/AKfycbzPL07GguXMLZVY5Pf_t2N5kQLdZkZgZdgUB_PMMo4NejTmufoQhA735lIsCW7xBxtnbA/exec";
